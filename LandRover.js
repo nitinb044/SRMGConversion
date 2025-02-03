3d9de6bdcb043688172ba5269bf05fb6b00c9074
nitinb044 <nitin.bhosle@srmg.com>
2025-02-03 17:06:09 +0400

    Update LandRover.js
    Signed-off-by: nitinb044 <nitin.bhosle@srmg.com>

--- LandRover.js
+++ LandRover.js
@@ -1,76 +1,19 @@
-// Custom HTML Tag Script for GTM
+(function() {
+    // Create the first iframe for the Land Rover pixel
+    var iframe1 = document.createElement('iframe');
+    iframe1.src = 'https://track.trackingfinder.com/success.php?offer_id=21897&afstatus=1';
+    iframe1.height = '1';
+    iframe1.width = '1';
+    iframe1.style.display = 'none'; // Hide the iframe
 
-// Wait for the DOM to be fully loaded
-document.addEventListener("DOMContentLoaded", function () {
-    // Static Pixels
-    (function () {
-        // Partner 1 - iframe
-        const partner1Iframe = document.createElement("iframe");
-        partner1Iframe.src = "https://arabyads.g2afse.com/success.php?offer_id=21897&afstatus=1";
-        partner1Iframe.width = "1";
-        partner1Iframe.height = "1";
-        partner1Iframe.style.display = "none";
-        partner1Iframe.style.border = "none";
-        document.body.appendChild(partner1Iframe);
-        console.log("Partner 1 iframe loaded.");
+    // Create the second iframe for the new pixel
+    var iframe2 = document.createElement('iframe');
+    iframe2.src = 'https://mm11007832.o18.link/p?m=23306&t=f&gb=1';
+    iframe2.width = '0px';
+    iframe2.height = '0px';
+    iframe2.style.display = 'none'; // Hide the iframe
 
-        // Partner 2 - image pixel
-        const partner2Img = document.createElement("img");
-        partner2Img.src = "https://mm11007832.o18.link/p?m=23306&t=i&gb=1";
-        partner2Img.width = 0;
-        partner2Img.height = 0;
-        partner2Img.style.display = "none";
-        document.body.appendChild(partner2Img);
-        console.log("Partner 2 image pixel loaded.");
-    })();
-
-    // Dynamic Pixels
-    (function () {
-        const pixelConfigs = [
-            // MM Pixel 1 - iframe
-            {
-                name: "MM Pixel 1",
-                type: "iframe",
-                id: "mm-pixel-1",
-                src: "https://mm11007832.o18.link/p?m=23306&t=f&gb=1",
-            },
-            // MM Pixel 2 - image
-            {
-                name: "MM Pixel 2",
-                type: "img",
-                id: "mm-pixel-2",
-                src: "https://mm11007832.o18.link/p?m=23306&t=i&gb=1",
-            },
-        ];
-
-        pixelConfigs.forEach((pixel) => {
-            if (pixel.type === "iframe") {
-                const iframe = document.createElement("iframe");
-                iframe.id = pixel.id;
-                iframe.src = pixel.src;
-                iframe.width = "0px";
-                iframe.height = "0px";
-                iframe.style.display = "none";
-                iframe.style.border = "none";
-                document.body.appendChild(iframe);
-                console.log(`${pixel.name} iframe loaded.`);
-            } else if (pixel.type === "img") {
-                const img = document.createElement("img");
-                img.id = pixel.id;
-                img.src = pixel.src;
-                img.width = 0;
-                img.height = 0;
-                img.style.display = "none";
-                document.body.appendChild(img);
-                console.log(`${pixel.name} image pixel loaded.`);
-            } else if (pixel.type === "script") {
-                const script = document.createElement("script");
-                script.id = pixel.id;
-                script.src = pixel.src;
-                script.async = true;
-                document.body.appendChild(script);
-                console.log(`${pixel.name} script loaded.`);
-            }
-        });
-    })();
-});
+    // Append both iframes to the document
+    document.head.appendChild(iframe1);
+    document.head.appendChild(iframe2);
+})();
